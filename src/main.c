36a354924a79292f032c8e6be7053f6617e571f0
William Yates <william.yates4@gmail.com>
2021-09-28 14:05:59 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,22 @@
-/* Bare-bones 1D FDTD simulation with a source. */
-
+/* 1D FDTD simulationb with an additive source */
 #include <stdio.h>
 #include <math.h>
 
 #define SIZE 200
 
- int main()
+int main()
  {
     double ez[SIZE] = {0.}, hy[SIZE] = {0.}, imp0 = 377.0;
-    int qTime, maxTime = 250, mm;
+    int qTime, maxTime = 200, mm;
+    /* base name set to "sim" but the user could be prompted to change this */
+    char basename[80] = "sim", filename[100];
+    int frame = 0;
+    /* file pointer used for output files */
+    FILE *snapshot;
 
      /* do time stepping */
      for (qTime = 0; qTime < maxTime; qTime++) {
+
 
          /* update magnetic field */
          for (mm = 0; mm < SIZE - 1; mm++)
@@ -21,11 +26,28 @@
          for (mm = 1; mm < SIZE; mm++)
          ez[mm] = ez[mm] + (hy[mm] - hy[mm - 1]) * imp0;
 
-         /* hardwire a source node */
-         ez[0] = exp(-(qTime - 30.) * (qTime - 30.) / 100.);
+         /* using an additive source at node 50 */
+         ez[50] += exp(-(qTime - 30.) * (qTime - 30.) / 100.);
+
+         /* create a snapshot if time-step is a multiple of 10*/
+         if (qTime % 10 == 0)
+         {
+            /* create file name and increment counter */
+            sprintf(filename, "%s.%d", basename, frame++);
+
+            snapshot = fopen(filename,"w");
+
+            /* write data to file*/
+            for(mm = 0; mm < SIZE; mm++)
+                fprintf(snapshot, "%g\n", ez[mm]);
+
+            fclose(snapshot);
+         }
 
      printf("%g\n", ez[50]);
  } /* end of time-stepping */
 
  return 0;
+}
+
 }
